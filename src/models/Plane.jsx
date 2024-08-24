8a66352ba34366d499e3e44d4a284478504ea458
parth2374 <parthbansal2374@gmail.com>
2024-08-24 20:55:19 +0530

    first commit

---
+++ src/models/Plane.jsx
@@ -0,0 +1,27 @@
+import React, { useRef, useEffect } from 'react'
+import { useAnimations, useGLTF } from '@react-three/drei'
+
+import planeScene from '../assets/3d/plane.glb'
+
+const Plane = ({ isRotating, ...props }) => {
+
+    const ref = useRef();
+    const { scene, animations } = useGLTF(planeScene);
+    const { actions } = useAnimations(animations, ref);
+
+    useEffect(() => {
+        if(isRotating) {
+            actions['Take 001'].play();
+        } else {
+            actions['Take 001'].stop();
+        }
+    }, [actions, isRotating])
+
+    return (
+        <mesh {...props}>
+            <primitive object={scene} ref={ref} />
+        </mesh>
+    )
+}
+
+export default Plane
